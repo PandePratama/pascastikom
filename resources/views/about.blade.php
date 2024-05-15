ed3bdd0b282293d1184c36488dfae7a040fbed8c
Pande Pratama <pratamaartha11@gmail.com>
2024-05-15 14:46:43 +0800

    menambahkan section sejarah

---
+++ resources/views/about.blade.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+<body>
+
+<!-- navbar -->
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark" style="background-color: #6C7ABB;" background-repeat: no-repeat; background-position: center; max-width: 1200px; margin: 0 auto;>
+    <a class="navbar-brand" href="#">
+    <img src="img/LogoITB.png" width="163" height="45" alt="logo">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ml-auto">
+            <a class="nav-link" href="#">About</a>
+            <a class="nav-link" href="#">Akademik</a>
+            <a class="nav-link" href="#">Penghargaan</a>
+            <a class="nav-link" href="#">Media</a>
+        </div>
+    </div>
+</nav>
+<!-- navbar end -->
+
+<!-- content -->
+<!-- sejarah -->
+<section class="container-fluid another-section" style="background-image: url('img/gedung.jpg'); background-size: cover; background-position: center; margin-bottom:0%; height:auto;">
+      <div class="container py-3">
+        <div class="idx text-center" style="background: rgba(255, 255, 255, 0.4); backdrop-filter: blur(5px); padding: 20px; border-radius: 10px;">
+          <h5 class="sejarah">Institut Teknologi dan Bisnis STIKOM Bali</h5>
+          <p class="content-paragraph text-justify">
+            Berawal dari bertemunya para pemerhati, pencinta dan praktisi pendidikan yakni Prof. Dr. Made Bandem, MA.,  (saat itu Rektor ISI Jogjakarta), Dr. Dadang Hermawan (praktisi pendidikan), Drs. Ida Bagus Dharmadiaksa, M.Si., Ak.
+            (Dosen) dan Drs. Satria Dharma (praktisi pendidikan) pada tahun 2.000 yang begitu menaruh perhatian atas pesat dan dinamisnya perkembangan teknologi informasi dan komunikasi (TIK) di dunia termasuk di Indonesia dan Bali, namun
+            di lain pihak perguruan tinggi bidang TIK sampai dengan jenjang sarjana belum ada.
+          </p>
+          <p class="content-paragraph text-justify">
+            Maka pada tanggal 20 Mei 2001, berdirilah Yayasan Widya Dharma Shanti yang  akan menjadi Badan Penyenggara Perguruan Tinggi Swasta dan selanjutnya diajukanlah ijin pendirian Institut Teknologi dan Bisnis (ITB) STIKOM Bali kepada
+            Direktorat Jenderal Pendidikan Tinggi Kementerian Pendidikan Nasional.
+          </p>
+          <p class="content-paragraph text-justify">
+            Dengan berbagai usaha dan lika liku pengurusan ijin serta doa, maka pada tanggal 10 Agustus 2002 keluarlah ijin dimaksud dengan nomor 157/D/O/2002 dengan 2 jurusan / program studi yakni Sistem Komputer (jenjang S1) dan Manajemen
+            Informatika (jenjang D3) lalu pada tahun 2009 ada tambahan program studi baru yakni Sistem Infomasi (jenjang S1).
+          </p>
+          <p class="content-paragraph text-justify mb-0">
+            Saat ini, Institut Teknologi dan Bisnis (ITB) STIKOM Bali sudah menjadi perguruan tinggi bertaraf internasional yang sangat dipercaya oleh masyarakat, terbukti dengan jumlah mahasiswa yang sedang kuliah tidak kurang dari 6.000
+            orang dan alumni yang mencapai hampir 4.000 orang (tahun 2015). Selain itu berbagai kerjasama dalam bidang tri dharma perguruan tinggi  telah dilakukan dengan berbagai pihak baik instansi pemerintah, instansi/perusahaan swasta,
+            BUMN, BUMD, Perguruan Tinggi baik dalam maupun luar negeri.
+          </p>
+        </div>
+      </div>
+    </section>
+    <!-- end sejarah -->
+
+    <!-- visi misi tujuan -->
+    <section class=" text-light" style="background-color: #0B1D6F; position:relative;">
+    <img src="img/ornamen.png" alt="Corner Image" class="corner-image top-left" style="position:absolute; top: 0px; left:0px; transform:rotate(180deg); width: 500px; height: 500px;">
+ 
+    <div class=" py-5 container">
+      <h3>Visi Direktorat Pascasarjana</h3>
+      <p>Menjadi penyelenggara program Pascasarjana yang berkualitas dan berdaya saing <br>internasional dalam pengembangan bidang ilmu Pengetahuan, Teknologi dan Seni</p>
+      <h3>Misi</h3>
+      <p>
+        1. Melaksanakan Tri Dharma Perguruan Tinggi secara profesional dan berkualitas. <br>
+        2. Berperan secara aktif dalam pengembangan di bidang ilmu pengetahuan, teknologi dan seni. <br>
+        3. Memberikan manfaat yang sebesar-besarnya kepada masyarakat sekitar, regional dan <br> nasional terkait dengan pengembangan  dan aplikasi bidang ilmu pengetahuan, teknologi <br>dan seni. <br>
+        4. Menjalin kerjasama dengan berbagai kalangan baik dalam maupun luar negeri dalam rangka <br>pengembangan dan peningkatan kualitas ITB STIKOM Bali. <br>
+        5. Menjadi wadah yang dapat dibanggakan dan memberikan rasa nyaman bagi seluruh warga <br>kampus. <br>
+        6. Melaksanakan pengelolaan institusi dengan memerhatikan prinsip-prinsip tata pamong dan <br> tata kelola yang baik (transparansi, akuntabilitas, bertanggungjawab, mandiri, dan berkeadilan).
+      </p>
+      <h3>Tujuan</h3>
+      <p>
+        1. Meningkatkan kualitas dan relevansi pendidikan di bidang ICT. <br>
+        2. Meningkatkan kualitas dan kuantitas penelitian dan pengabdian kepada masyarakat di <br>bidang ICT. <br>
+        3. Menumbuhkan sikap toleransi, kemampuan kerjasama, semangat kepribadian bangsa, serta <br>mengembangkan displin di kalangan civitas akademika dan masyarakat. <br>
+        4. Mengembangkan pengeloalaan Institut Teknologi dan Bisnis (ITB) STIKOM Bali yang efisien <br>dan produktif di dukung oleh sarana dan prasarana yang memadai dan bermutu. <br>
+        5. Mengembangkan jaringan kerjasama dengan masyarakat kampus, alumni, industri, dan <br>masyarakat. <br>
+      </p>
+      </div>
+      <img src="img/ornamen.png" alt="Corner Image" class="corner-image bottom-right" style="position:absolute; bottom:0px; right:0px; width: 500px; height: 500px;">
+    </section>
+
+    <!-- struktur organisasi -->
+    
+
+<!-- end content -->
+
+<!-- footer -->
+<!-- <div class="card text-center"> -->
+  <div class="card-body text-center p-2" style="background-color: #D9D9D9; margin-top: 0px;">
+    <h5 class="card-title">Alamat Kampus</h5>
+    <div class="d-flex justify-content-center">
+        <div class="akkiri mx-3">
+            <img src="img/Group 1.png" alt="">
+            <p class="text-left">Kampus Terakreditasi B oleh BAN-PT dan<br>siap mendidik mahasiswa dalam bidang<br>Teknologi Indormasi</p>
+        </div>
+        <div class="akkanan mx-3">
+                <p class="text-left">Kampus Renon: <br>Jl. Raya Puputan No. 86 Renon – Denpasar <br>Telp: (0361) 244445</p>
+                <p class="text-left">Kampus Jimbaran: <br>Jl. Raya Kampus Udayana Jimbaran Bali <br>Telp : (0361) 8953537</p>
+                <p class="text-left">Kampus Abiansemal: <br>Jl. Janger Dauh Yeh Cani Kec. Abiansemal <br>Kabupaten Badung, Bali <br>Telp : 0856-3700-803</p>
+        </div>
+    </div>
+    <!-- <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a> -->
+  </div>
+
+  <!-- footer -->
+  <div class="card-footer text-muted" style="background-color: #6C7ABB; display: flex; justify-content: space-between; align-items: center; bottom: 0;">
+    <div class="social-media">
+        <a href="#"><i class="bi bi-instagram text-white mx-2" style="font-size: 35px;"></i></a>
+        <a href="#"><i class="bi bi-facebook text-white mx-2" style="font-size: 35px;"></i></a>
+        <a href="#"><i class="bi bi-twitter text-white mx-2" style="font-size: 35px;"></i></a>
+    </div>
+    <div class="copyright" style="color: white;">
+        &copy; All Right Reserved 2024, Pusat Komputer dan Jaringan ITB STIKOM Bali
+    </div>
+  </div>
+
+<!-- end footer -->
+
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'Hello World';
+Route::get('/about', function () {
+    return view('about');
 });

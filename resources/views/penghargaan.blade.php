775e4501696b75f1c00990bc6214931efe1bd7be
egsyoo <hergiano2000@gmail.com>
2024-05-22 00:07:55 +0800

    Membuat halaman penghargaan

---
+++ resources/views/penghargaan.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <title>Penghargaan</title>
+</head>
+
+<body>
+  <!-- navbar -->
+  <x-navbar></x-navbar>
+  <!-- navbar end -->
+  <img src="/img/stikombanner.jpeg" alt="posterstikom" style="display: block; margin-left: auto; margin-right: auto;">
+  <p class="text-justify" style="margin: 25px 50px 75px 100px;">ITB Stikom Bali senantiasa berjuang untuk dapat segera mewujudkan visi-nya. Perjuangan ITB Stikom Bali membuahkan hasil dengan menorehkan berbagai prestasi dalam proses pengukuran kinerja perguruan tinggi. Beragam prestasi dan hibah kompetisi juga berhasil dimenangkan oleh ITB Stikom Bali. Semua torehan tersebut tentu tidak lepas dari kerja keras dan kerja cerdas seluruh sivitas akademik ITB Stikom Bali. Hal ini merupakan wujud nyata komitmen semua sivitas dalam bekerja dengan kebersamaan untuk mewujudkan visi Undiksha. Peringkat dan penghargaan bukan merupakan tujuan utama bagi Undiksha, tetapi yang terpenting adalah bagaiamana proses yang dilalui untuk selalu meningkatkan kualitas mutu kinerja bersama.</p>
+
+  <h3 class="text-justify" style="margin: 25px 50px 75px 100px;">Akreditasi Program Studi Sistem Informasi Pada Program Magister Institut Teknologi Dan Bisnis Stikom Bali</h3>
+
+  <img src="/img/banpt.png" alt="posterstikom" style="display: block; margin-left: auto; margin-right: auto; margin: bottom 50px;">
+  
+  <p class="text-justify" style="margin: 10px 50px 75px 100px;">Badan Akreditasi Nasional Perguruan Tinggi (BAN-PT), Kamis (21/2) secara resmi mengeluarkan Lembaga Institut Teknologi Stikom Bali Dengan status hasil akrditasi B. Keputusan menggembirakan seluruh civitas akademika itu tersurat dalam keputusan BAN-PT No.3316/SK/BAN-PT/Ak.P/M/VIII/2023. Akreditasi sudah dilakukan pada 2023 dan menjadi komitmen civitas akademika. Capaian akreditasi merupakan peningkatan prestasi dan kinerja seluruh elemen.</p>
+
+  <img src="/img/akreditasi.png" alt="posterstikom" style="display: block; margin-left: auto; margin-right: auto; margin-bottom: 50px;">
+
+  <!-- footer -->
+  <x-footer></x-footer>
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+</body>
+
+</html>

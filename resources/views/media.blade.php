93d3274a6c60cd1566f02cfaece8e3eb56faf5f1
Pande Pratama <pratamaartha11@gmail.com>
2024-05-22 12:30:21 +0800

    bug fixing all design

--- resources/views/media.blade.php
+++ resources/views/media.blade.php
@@ -1,90 +1,92 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <title>Hello, world!</title>
-  </head>
-  <body>
+    <title>Media</title>
+</head>
+
+<body>
     <!-- navbar -->
     <x-navbar></x-navbar>
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-6 col-lg-4 mb-1 mt-2 pr-0">
-            <div class="card" style="display: flex; flex-direction: row; align-items: flex-start; height: 20rem;">
-                <img src="img/image 19.png" class="card-img-top img-fluid" alt="..." style="width: 50%; height:auto; object-fit:cover;">
-                <div class="card-body" style="display:flex; flex-direction:column; justify-content:center;width:50%; padding:0;">
-                    <h5 class="card-title" style="font-size: medium;">MAGISTER KOMPUTER S2 PROGRAM STUDI SISTEM INFORMASI ITB STIKOM BALI</h5>
-                    <p style="font-size: small;">Kamu bisa kuliah 1,5 tahun dengan biaya yang terjangkau, fasilitas yang lengkap dan jadwal kuliah yang Fleksibel</p>
-                    <p style="font-size: small;">Ayo wujudkan mimpi dan masa depanmu sesuai passionmu bersama ITB STIKOM Bali!</p>
-                    <a href="#" class="btn btn-primary">Selengkapnya</a>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-4 mb-1 mt-2 pr-0">
+                <div class="card" style="display: flex; flex-direction: row; align-items: flex-start; height: 20rem;">
+                    <img src="img/image 19.png" class="card-img-top img-fluid" alt="..." style="width: 50%; height:auto; object-fit:cover;">
+                    <div class="card-body" style="display:flex; flex-direction:column; justify-content:center;width:50%; padding:0;">
+                        <h5 class="card-title" style="font-size: medium;">MAGISTER KOMPUTER S2 PROGRAM STUDI SISTEM INFORMASI ITB STIKOM BALI</h5>
+                        <p style="font-size: small;">Kamu bisa kuliah 1,5 tahun dengan biaya yang terjangkau, fasilitas yang lengkap dan jadwal kuliah yang Fleksibel</p>
+                        <p style="font-size: small;">Ayo wujudkan mimpi dan masa depanmu sesuai passionmu bersama ITB STIKOM Bali!</p>
+                        <a href="#" class="btn btn-primary">Selengkapnya</a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-6 col-lg-4 mb-1 mt-2 px-0">
-            <div class="card" style="display: flex; flex-direction: row; align-items: flex-start; height: 20rem;">
-                <img src="img/image 20.png" class="card-img-top img-fluid" alt="..." style="width: 50%; height:auto; object-fit:cover;">
-                <div class="card-body" style="display:flex; flex-direction:column; justify-content:center;width:50%; padding:0;">
-                    <h5 class="card-title" style="font-size: medium;">MAGISTER KOMPUTER S2 PROGRAM STUDI SISTEM INFORMASI ITB STIKOM BALI</h5>
-                    <p style="font-size: small;">Kamu bisa kuliah 1,5 tahun dengan biaya yang terjangkau, fasilitas yang lengkap dan jadwal kuliah yang Fleksibel</p>
-                    <p style="font-size: small;">Ayo wujudkan mimpi dan masa depanmu sesuai passionmu bersama ITB STIKOM Bali!</p>
-                    <a href="#" class="btn btn-primary">Selengkapnya</a>
+            <div class="col-md-6 col-lg-4 mb-1 mt-2 px-0">
+                <div class="card" style="display: flex; flex-direction: row; align-items: flex-start; height: 20rem;">
+                    <img src="img/image 20.png" class="card-img-top img-fluid" alt="..." style="width: 50%; height:auto; object-fit:cover;">
+                    <div class="card-body" style="display:flex; flex-direction:column; justify-content:center;width:50%; padding:0;">
+                        <h5 class="card-title" style="font-size: medium;">MAGISTER KOMPUTER S2 PROGRAM STUDI SISTEM INFORMASI ITB STIKOM BALI</h5>
+                        <p style="font-size: small;">Kamu bisa kuliah 1,5 tahun dengan biaya yang terjangkau, fasilitas yang lengkap dan jadwal kuliah yang Fleksibel</p>
+                        <p style="font-size: small;">Ayo wujudkan mimpi dan masa depanmu sesuai passionmu bersama ITB STIKOM Bali!</p>
+                        <a href="#" class="btn btn-primary">Selengkapnya</a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-6 col-lg-4 mb-1 mt-2 pl-0">
-            <div class="card" style="display: flex; flex-direction: row; align-items: flex-start; height: 20rem;">
-                <img src="img/image 23.png" class="card-img-top img-fluid" alt="..." style="width: 50%; height:auto; object-fit:cover;">
-                <div class="card-body" style="display:flex; flex-direction:column; justify-content:center;width:50%; padding:0;">
-                    <h5 class="card-title" style="font-size: medium;">MAGISTER KOMPUTER S2 PROGRAM STUDI SISTEM INFORMASI ITB STIKOM BALI</h5>
-                    <p style="font-size: small;">Kamu bisa kuliah 1,5 tahun dengan biaya yang terjangkau, fasilitas yang lengkap dan jadwal kuliah yang Fleksibel</p>
-                    <p style="font-size: small;">Ayo wujudkan mimpi dan masa depanmu sesuai passionmu bersama ITB STIKOM Bali!</p>
-                    <a href="#" class="btn btn-primary">Selengkapnya</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-6 col-lg-4 mb-1 mt-2 pr-0">
-            <div class="card" style="display: flex; flex-direction: row; align-items: flex-start; height: 20rem;">
-                <img src="img/image 21.png" class="card-img-top img-fluid" alt="..." style="width: 50%; height:auto; object-fit:cover;">
-                <div class="card-body" style="display:flex; flex-direction:column; justify-content:center;width:50%; padding:0;">
-                    <h5 class="card-title" style="font-size: medium;">MAGISTER KOMPUTER S2 PROGRAM STUDI SISTEM INFORMASI ITB STIKOM BALI</h5>
-                    <p style="font-size: small;">Kamu bisa kuliah 1,5 tahun dengan biaya yang terjangkau, fasilitas yang lengkap dan jadwal kuliah yang Fleksibel</p>
-                    <p style="font-size: small;">Ayo wujudkan mimpi dan masa depanmu sesuai passionmu bersama ITB STIKOM Bali!</p>
-                    <a href="#" class="btn btn-primary">Selengkapnya</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6 col-lg-4 mb-1 mt-2 px-0">
-            <div class="card" style="display: flex; flex-direction: row; align-items: flex-start; height: 20rem;">
-                <img src="img/image 22.png" class="card-img-top img-fluid" alt="..." style="width: 50%; height:auto; object-fit:cover;">
-                <div class="card-body" style="display:flex; flex-direction:column; justify-content:center;width:50%; padding:0;">
-                    <h5 class="card-title" style="font-size: medium;">MAGISTER KOMPUTER S2 PROGRAM STUDI SISTEM INFORMASI ITB STIKOM BALI</h5>
-                    <p style="font-size: small;">Kamu bisa kuliah 1,5 tahun dengan biaya yang terjangkau, fasilitas yang lengkap dan jadwal kuliah yang Fleksibel</p>
-                    <p style="font-size: small;">Ayo wujudkan mimpi dan masa depanmu sesuai passionmu bersama ITB STIKOM Bali!</p>
-                    <a href="#" class="btn btn-primary">Selengkapnya</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6 col-lg-4 mb-1 mt-2 pl-0">
-            <div class="card" style="display: flex; flex-direction: row; align-items: flex-start; height: 20rem;">
-                <img src="img/image 24.png" class="card-img-top img-fluid" alt="..." style="width: 50%; height:auto; object-fit:cover;">
-                <div class="card-body" style="display:flex; flex-direction:column; justify-content:center;width:50%; padding:0;">
-                    <h5 class="card-title" style="font-size: medium;">MAGISTER KOMPUTER S2 PROGRAM STUDI SISTEM INFORMASI ITB STIKOM BALI</h5>
-                    <p style="font-size: small;">Kamu bisa kuliah 1,5 tahun dengan biaya yang terjangkau, fasilitas yang lengkap dan jadwal kuliah yang Fleksibel</p>
-                    <p style="font-size: small;">Ayo wujudkan mimpi dan masa depanmu sesuai passionmu bersama ITB STIKOM Bali!</p>
-                    <a href="#" class="btn btn-primary">Selengkapnya</a>
+            <div class="col-md-6 col-lg-4 mb-1 mt-2 pl-0">
+                <div class="card" style="display: flex; flex-direction: row; align-items: flex-start; height: 20rem;">
+                    <img src="img/image 23.png" class="card-img-top img-fluid" alt="..." style="width: 50%; height:auto; object-fit:cover;">
+                    <div class="card-body" style="display:flex; flex-direction:column; justify-content:center;width:50%; padding:0;">
+                        <h5 class="card-title" style="font-size: medium;">MAGISTER KOMPUTER S2 PROGRAM STUDI SISTEM INFORMASI ITB STIKOM BALI</h5>
+                        <p style="font-size: small;">Kamu bisa kuliah 1,5 tahun dengan biaya yang terjangkau, fasilitas yang lengkap dan jadwal kuliah yang Fleksibel</p>
+                        <p style="font-size: small;">Ayo wujudkan mimpi dan masa depanmu sesuai passionmu bersama ITB STIKOM Bali!</p>
+                        <a href="#" class="btn btn-primary">Selengkapnya</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-4 mb-1 mt-2 pr-0">
+                <div class="card" style="display: flex; flex-direction: row; align-items: flex-start; height: 20rem;">
+                    <img src="img/image 21.png" class="card-img-top img-fluid" alt="..." style="width: 50%; height:auto; object-fit:cover;">
+                    <div class="card-body" style="display:flex; flex-direction:column; justify-content:center;width:50%; padding:0;">
+                        <h5 class="card-title" style="font-size: medium;">MAGISTER KOMPUTER S2 PROGRAM STUDI SISTEM INFORMASI ITB STIKOM BALI</h5>
+                        <p style="font-size: small;">Kamu bisa kuliah 1,5 tahun dengan biaya yang terjangkau, fasilitas yang lengkap dan jadwal kuliah yang Fleksibel</p>
+                        <p style="font-size: small;">Ayo wujudkan mimpi dan masa depanmu sesuai passionmu bersama ITB STIKOM Bali!</p>
+                        <a href="#" class="btn btn-primary">Selengkapnya</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-1 mt-2 px-0">
+                <div class="card" style="display: flex; flex-direction: row; align-items: flex-start; height: 20rem;">
+                    <img src="img/image 22.png" class="card-img-top img-fluid" alt="..." style="width: 50%; height:auto; object-fit:cover;">
+                    <div class="card-body" style="display:flex; flex-direction:column; justify-content:center;width:50%; padding:0;">
+                        <h5 class="card-title" style="font-size: medium;">MAGISTER KOMPUTER S2 PROGRAM STUDI SISTEM INFORMASI ITB STIKOM BALI</h5>
+                        <p style="font-size: small;">Kamu bisa kuliah 1,5 tahun dengan biaya yang terjangkau, fasilitas yang lengkap dan jadwal kuliah yang Fleksibel</p>
+                        <p style="font-size: small;">Ayo wujudkan mimpi dan masa depanmu sesuai passionmu bersama ITB STIKOM Bali!</p>
+                        <a href="#" class="btn btn-primary">Selengkapnya</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-1 mt-2 pl-0">
+                <div class="card" style="display: flex; flex-direction: row; align-items: flex-start; height: 20rem;">
+                    <img src="img/image 24.png" class="card-img-top img-fluid" alt="..." style="width: 50%; height:auto; object-fit:cover;">
+                    <div class="card-body" style="display:flex; flex-direction:column; justify-content:center;width:50%; padding:0;">
+                        <h5 class="card-title" style="font-size: medium;">MAGISTER KOMPUTER S2 PROGRAM STUDI SISTEM INFORMASI ITB STIKOM BALI</h5>
+                        <p style="font-size: small;">Kamu bisa kuliah 1,5 tahun dengan biaya yang terjangkau, fasilitas yang lengkap dan jadwal kuliah yang Fleksibel</p>
+                        <p style="font-size: small;">Ayo wujudkan mimpi dan masa depanmu sesuai passionmu bersama ITB STIKOM Bali!</p>
+                        <a href="#" class="btn btn-primary">Selengkapnya</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
     <!-- footer -->
@@ -92,5 +94,6 @@
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
--- resources/views/penghargaan.blade.php
+++ resources/views/penghargaan.blade.php
@@ -13,16 +13,14 @@
   <!-- navbar -->
   <x-navbar></x-navbar>
   <!-- navbar end -->
-  <img src="/img/stikombanner.jpeg" alt="posterstikom" style="display: block; margin-left: auto; margin-right: auto;">
-  <p class="text-justify" style="margin: 25px 50px 75px 100px;">ITB Stikom Bali senantiasa berjuang untuk dapat segera mewujudkan visi-nya. Perjuangan ITB Stikom Bali membuahkan hasil dengan menorehkan berbagai prestasi dalam proses pengukuran kinerja perguruan tinggi. Beragam prestasi dan hibah kompetisi juga berhasil dimenangkan oleh ITB Stikom Bali. Semua torehan tersebut tentu tidak lepas dari kerja keras dan kerja cerdas seluruh sivitas akademik ITB Stikom Bali. Hal ini merupakan wujud nyata komitmen semua sivitas dalam bekerja dengan kebersamaan untuk mewujudkan visi Undiksha. Peringkat dan penghargaan bukan merupakan tujuan utama bagi Undiksha, tetapi yang terpenting adalah bagaiamana proses yang dilalui untuk selalu meningkatkan kualitas mutu kinerja bersama.</p>
 
-  <h3 class="text-justify" style="margin: 25px 50px 75px 100px;">Akreditasi Program Studi Sistem Informasi Pada Program Magister Institut Teknologi Dan Bisnis Stikom Bali</h3>
-
-  <img src="/img/banpt.png" alt="posterstikom" style="display: block; margin-left: auto; margin-right: auto; margin: bottom 50px;">
-  
-  <p class="text-justify" style="margin: 10px 50px 75px 100px;">Badan Akreditasi Nasional Perguruan Tinggi (BAN-PT), Kamis (21/2) secara resmi mengeluarkan Lembaga Institut Teknologi Stikom Bali Dengan status hasil akrditasi B. Keputusan menggembirakan seluruh civitas akademika itu tersurat dalam keputusan BAN-PT No.3316/SK/BAN-PT/Ak.P/M/VIII/2023. Akreditasi sudah dilakukan pada 2023 dan menjadi komitmen civitas akademika. Capaian akreditasi merupakan peningkatan prestasi dan kinerja seluruh elemen.</p>
-
-  <img src="/img/akreditasi.png" alt="posterstikom" style="display: block; margin-left: auto; margin-right: auto; margin-bottom: 50px;">
+  <div id="penghargaan" class="container text-center py-3">
+    <img src="/img/stikombanner.jpeg" alt="" class="mb-3">
+    <p class="text-justify">ITB Stikom Bali senantiasa berjuang untuk dapat segera mewujudkan visi-nya. Perjuangan ITB Stikom Bali membuahkan hasil dengan menorehkan berbagai prestasi dalam proses pengukuran kinerja perguruan tinggi. Beragam prestasi dan hibah kompetisi juga berhasil dimenangkan oleh ITB Stikom Bali. Semua torehan tersebut tentu tidak lepas dari kerja keras dan kerja cerdas seluruh sivitas akademik ITB Stikom Bali. Hal ini merupakan wujud nyata komitmen semua sivitas dalam bekerja dengan kebersamaan untuk mewujudkan visi Undiksha. Peringkat dan penghargaan bukan merupakan tujuan utama bagi Undiksha, tetapi yang terpenting adalah bagaiamana proses yang dilalui untuk selalu meningkatkan kualitas mutu kinerja bersama.</p>
+    <p class="text-center font-weight-bold">Akreditasi Program Studi Sistem Informasi Pada Program Magister Institut Teknologi Dan Bisnis Stikom Bali</p>
+    <img src="/img/banpt.png" alt="">
+    <p class="text-justify">Badan Akreditasi Nasional Perguruan Tinggi (BAN-PT), Kamis (21/2) secara resmi mengeluarkan Lembaga Institut Teknologi Stikom Bali Dengan status hasil akrditasi B. Keputusan menggembirakan seluruh civitas akademika itu tersurat dalam keputusan BAN-PT No.3316/SK/BAN-PT/Ak.P/M/VIII/2023. Akreditasi sudah dilakukan pada 2023 dan menjadi komitmen civitas akademika. Capaian akreditasi merupakan peningkatan prestasi dan kinerja seluruh elemen.</p>
+  </div>
 
   <!-- footer -->
   <x-footer></x-footer>

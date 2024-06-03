86ae3daaa06b87828ba89dd5b07003e35266508b
Pande Pratama <pratamaartha11@gmail.com>
2024-06-03 14:42:05 +0800

    update hover

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,6 +7,7 @@
   <title>About</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -18,19 +19,19 @@
   <!-- content -->
   <!-- sejarah -->
   <section style="background-image: url('img/gedung.jpg'); background-size: cover; background-position: center; height: 100%;">
-    <div class="container-fluid h-100">
-      <div class="idx text-center d-flex flex-column justify-content-center align-items-center" style="background: rgba(255, 255, 255, 0.4); backdrop-filter: blur(5px); padding: 20px; height: 100%;">
-        <h5 id="sejarah" class="mt-5">Institut Teknologi dan Bisnis STIKOM Bali</h5>
-        <p class="content-paragraph mx-3 mx-md-5">
+    <div class=" h-100">
+      <div class="idx text-center d-flex flex-column justify-content-center align-items-center" style="background: rgba(255, 255, 255, 0.8); backdrop-filter: blur(10px); padding: 20px; height: 100%;">
+        <h2 id="sejarah" class="mt-5">Institut Teknologi dan Bisnis STIKOM Bali</h2>
+        <p class="content-paragraph mx-3 mx-md-5" style="font-size: large;">
           Berawal dari bertemunya para pemerhati, pencinta dan praktisi pendidikan yakni Prof. Dr. Made Bandem, MA., (saat itu Rektor ISI Jogjakarta), Dr. Dadang Hermawan (praktisi pendidikan), Drs. Ida Bagus Dharmadiaksa, M.Si., Ak. (Dosen) dan Drs. Satria Dharma (praktisi pendidikan) pada tahun 2.000 yang begitu menaruh perhatian atas pesat dan dinamisnya perkembangan teknologi informasi dan komunikasi (TIK) di dunia termasuk di Indonesia dan Bali, namun di lain pihak perguruan tinggi bidang TIK sampai dengan jenjang sarjana belum ada.
         </p>
-        <p class="content-paragraph mx-3 mx-md-5">
+        <p class="content-paragraph mx-3 mx-md-5" style="font-size: large;">
           Maka pada tanggal 20 Mei 2001, berdirilah Yayasan Widya Dharma Shanti yang akan menjadi Badan Penyenggara Perguruan Tinggi Swasta dan selanjutnya diajukanlah ijin pendirian Institut Teknologi dan Bisnis (ITB) STIKOM Bali kepada Direktorat Jenderal Pendidikan Tinggi Kementerian Pendidikan Nasional.
         </p>
-        <p class="content-paragraph mx-3 mx-md-5">
+        <p class="content-paragraph mx-3 mx-md-5" style="font-size: large;">
           Dengan berbagai usaha dan lika liku pengurusan ijin serta doa, maka pada tanggal 10 Agustus 2002 keluarlah ijin dimaksud dengan nomor 157/D/O/2002 dengan 2 jurusan / program studi yakni Sistem Komputer (jenjang S1) dan Manajemen Informatika (jenjang D3) lalu pada tahun 2009 ada tambahan program studi baru yakni Sistem Informasi (jenjang S1).
         </p>
-        <p class="content-paragraph mx-3 mx-md-5">
+        <p class="content-paragraph mx-3 mx-md-5" style="font-size: large;">
           Saat ini, Institut Teknologi dan Bisnis (ITB) STIKOM Bali sudah menjadi perguruan tinggi bertaraf internasional yang sangat dipercaya oleh masyarakat, terbukti dengan jumlah mahasiswa yang sedang kuliah tidak kurang dari 6.000 orang dan alumni yang mencapai hampir 4.000 orang (tahun 2015). Selain itu berbagai kerjasama dalam bidang tri dharma perguruan tinggi telah dilakukan dengan berbagai pihak baik instansi pemerintah, instansi/perusahaan swasta, BUMN, BUMD, Perguruan Tinggi baik dalam maupun luar negeri.
         </p>
       </div>
@@ -40,7 +41,7 @@
 
   <!-- visi misi tujuan -->
   <section id="visi-misi" class="text-light position-relative" style="background-color: #0B1D6F; position: relative;">
-    <img src="img/ornamen.png" alt="Corner Image" class="corner-image top-image" style="transform: rotate(180deg); width: 300px; height: 400px; position: absolute; top: 0; left: 0;">
+    <img src="img/ornamen.png" alt="Corner Image" class="corner-image top-image" style="transform: rotate(180deg); width: 300px; height: 300px; position: absolute; top: 0; left: 0;">
     <div class="container py-5">
       <h3>Visi Direktorat Pascasarjana</h3>
       <p>Menjadi penyelenggara program Pascasarjana yang berkualitas dan berdaya saing internasional dalam pengembangan bidang ilmu Pengetahuan, Teknologi dan Seni</p>
@@ -64,7 +65,7 @@
         5. Mengembangkan jaringan kerjasama dengan masyarakat kampus, alumni, industri, dan masyarakat. <br>
       </p>
     </div>
-    <img src="img/ornamen.png" alt="Corner Image" class="corner-image bottom-image" style="width: 300px; height: 400px; position: absolute; bottom: 0; right: 0;">
+    <img src="img/ornamen.png" alt="Corner Image" class="corner-image bottom-image" style="width: 300px; height: 300px; position: absolute; bottom: 0; right: 0;">
   </section>
 
 
@@ -78,6 +79,7 @@
   </div>
 
   <section id="" class="text-light position-relative" style="background-color: #0B1D6F; position: relative;">
+    <img src="img/ornamen.png" alt="Corner Image" class="corner-image top-image" style="transform: rotate(180deg); width: 300px; height: 300px; position: absolute; top: 0; left: 0;">
     <div class="container mb-5 pt-5">
       <div class="row rounded flex-column align-items-center" style="background-color: #D9D9D9; color:black;">
         <div class="col-md-4 text-center mb-4">
@@ -109,10 +111,58 @@
         </div>
       </div>
     </div>
+    <img src="img/ornamen.png" alt="Corner Image" class="corner-image bottom-image" style="width: 300px; height: 300px; position: absolute; bottom: 0; right: 0;">
+  </section>
 
-    <!-- end content -->
+  <!-- Dosen Pengajar -->
+  <div class="container text-center border rounded mb-5 header-container" style="width: 100%; max-width: 250px; height: 50px; background-color:#6C7ABB; color:black; font-weight: bold; padding: top 2px;">
+    Direktori Dosen Pengajar
+  </div>
 
-    <!-- footer -->
-    <x-footer></x-footer>
+  <!-- Image Grid -->
+  <div class="container">
+    <div class="img-container">
+      <div class="img-wrapper">
+        <img src="img/dosen1.png" alt="Dosen 1">
+        <span class="img-name">Prof. Dr. I Made Bandem</span>
+      </div>
+      <div class="img-wrapper">
+        <img src="img/dosen2.png" alt="Dosen 2">
+        <span class="img-name">Dr. Dadang Hermawan</span>
+      </div>
+      <div class="img-wrapper">
+        <img src="img/dosen3.png" alt="Dosen 3">
+        <span class="img-name">Dr. Roy Rudolf Huizen, S.T., M.T</span>
+      </div>
+      <div class="img-wrapper">
+        <img src="img/dosen4.png" alt="Dosen 4">
+        <span class="img-name">Dr. Dandy Pramana Hostiadi, S.Kom., M.T</span>
+      </div>
+    </div>
+    <div class="img-container">
+      <div class="img-wrapper">
+        <img src="img/dosen5.png" alt="Dosen 5">
+        <span class="img-name">Dr. Putri Srinadi, S.E, M.M.Kom</span>
+      </div>
+      <div class="img-wrapper">
+        <img src="img/dosen6.png" alt="Dosen 6">
+        <span class="img-name">Dr. Putu Desiana Wulaning Ayu., S.T., M.T</span>
+      </div>
+      <div class="img-wrapper">
+        <img src="img/dosen7.png" alt="Dosen 7">
+        <span class="img-name">Dr. Evi Triandini, S.P., M.Eng
+        </span>
+      </div>
+      <div class="img-wrapper">
+        <img src="img/dosen8.png" alt="Dosen 8">
+        <span class="img-name">Dr. Gede Angga Pradipta, S.T., M.Eng</span>
+      </div>
+    </div>
+  </div>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha
+  <!-- end content -->
+
+  <!-- footer -->
+  <x-footer></x-footer>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar sticky-top navbar-expand-lg navbar-dark" style="background-color: #6C7ABB;" background-repeat: no-repeat; background-position: center; max-width: 1200px; margin: 0 auto;>
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark shadow-lg" style="background-color: #6C7ABB;" background-repeat: no-repeat; background-position: center; max-width: 1200px; margin: 0 auto;>
   <a class="navbar-brand" href="#">
     <img src="img/LogoITB.png" width="163" height="45" alt="logo">
   </a>
@@ -11,21 +11,21 @@
         <a class="nav-link dropdown" href="{{ url('/about')}}" role="button" data-toggle="dropdown" aria-expanded="false">
           About
         </a>
-        <div class="dropdown-menu">
-          <a class="dropdown-item" href="{{ url('/about#sejarah')}}">Sejarah</a>
-          <a class="dropdown-item" href="{{ url('/about#visi-misi')}}">Visi Misi</a>
-          <a class="dropdown-item" href="{{ url('/about#struktur')}}">Struktur</a>
+        <div class="dropdown-menu" style="background-color:#6C7ABB;">
+          <a class="dropdown-item mb-2" style="background-color: #D9D9D9; border-radius:20px; text-align:center; padding:0; width: 100px" href="{{ url('/about#sejarah')}}">Sejarah</a>
+          <a class="dropdown-item mb-2" style="background-color: #D9D9D9; border-radius:20px; text-align:center; padding:0; width: 100px" href="{{ url('/about#visi-misi')}}">Visi Misi</a>
+          <a class="dropdown-item" style="background-color: #D9D9D9; border-radius:20px; text-align:center; padding:0; width: 100px" href="{{ url('/about#struktur')}}">Struktur</a>
         </div>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown" href="{{ url('/akademik')}}" role="button" data-toggle="dropdown" aria-expanded="false">
           Akademik
         </a>
-        <div class="dropdown-menu">
-          <a class="dropdown-item" href="{{ url('/akademik#peresmian')}}">Peresmian</a>
-          <a class="dropdown-item" href="{{ url('/akademik#konsentrasi')}}">Konsentrasi</a>
-          <a class="dropdown-item" href="{{ url('/akademik#biaya')}}">Biaya</a>
-          <a class="dropdown-item" href="{{ url('/akademik#persyaratan')}}">Persyaratan</a>
+        <div class="dropdown-menu" style="background-color:#6C7ABB;">
+          <a class="dropdown-item mb-2" style="background-color: #D9D9D9; border-radius:20px; text-align:center; padding:0;" href="{{ url('/akademik#peresmian')}}">Peresmian</a>
+          <a class="dropdown-item mb-2" style="background-color: #D9D9D9; border-radius:20px; text-align:center; padding:0;" href="{{ url('/akademik#konsentrasi')}}">Konsentrasi</a>
+          <a class="dropdown-item mb-2" style="background-color: #D9D9D9; border-radius:20px; text-align:center; padding:0;" href="{{ url('/akademik#biaya')}}">Biaya</a>
+          <a class="dropdown-item" style="background-color: #D9D9D9; border-radius:20px; text-align:center; padding:0;" href="{{ url('/akademik#persyaratan')}}">Persyaratan</a>
         </div>
       </li>
       <a class="nav-link" href="{{ url('/penghargaan')}}">Penghargaan</a>

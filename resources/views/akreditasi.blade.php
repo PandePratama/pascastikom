b437787af19fe4ec93401167516795092aaed6e6
AdityaArtamerta <artamertaaditya@gmail.com>
2024-08-01 10:43:42 +0800

    revisi tgl 1-8-24

--- resources/views/akreditasi.blade.php
+++ resources/views/akreditasi.blade.php
@@ -4,7 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+    integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="css/style.css">
   <title>Akreditasi</title>
@@ -15,19 +16,40 @@
   <x-navbar></x-navbar>
   <!-- navbar end -->
 
-  <div id="akreditasi" class="container text-center py-3">
-    <img src="/img/stikombanner.jpeg" alt="" class="img-fluid mb-3">
-    <p class="text-justify">ITB Stikom Bali senantiasa berjuang untuk dapat segera mewujudkan visi-nya. Perjuangan ITB Stikom Bali membuahkan hasil dengan menorehkan berbagai prestasi dalam proses pengukuran kinerja perguruan tinggi. Beragam prestasi dan hibah kompetisi juga berhasil dimenangkan oleh ITB Stikom Bali. Semua torehan tersebut tentu tidak lepas dari kerja keras dan kerja cerdas seluruh sivitas akademik ITB Stikom Bali. Hal ini merupakan wujud nyata komitmen semua sivitas dalam bekerja dengan kebersamaan untuk mewujudkan visi Undiksha. Peringkat dan penghargaan bukan merupakan tujuan utama bagi Undiksha, tetapi yang terpenting adalah bagaiamana proses yang dilalui untuk selalu meningkatkan kualitas mutu kinerja bersama.</p>
-    <p class="text-center font-weight-bold">Akreditasi Program Studi Sistem Informasi Pada Program Magister Institut Teknologi Dan Bisnis Stikom Bali</p>
-    <img src="/img/banpt.png" alt="">
-    <p class="text-justify">Badan Akreditasi Nasional Perguruan Tinggi (BAN-PT), Kamis (21/2) secara resmi mengeluarkan Lembaga Institut Teknologi Stikom Bali Dengan status hasil akrditasi B. Keputusan menggembirakan seluruh civitas akademika itu tersurat dalam keputusan BAN-PT No.3316/SK/BAN-PT/Ak.P/M/VIII/2023. Akreditasi sudah dilakukan pada 2023 dan menjadi komitmen civitas akademika. Capaian akreditasi merupakan peningkatan prestasi dan kinerja seluruh elemen.</p>
-    <img src="/img/akreditasi.png" class="img-fluid" alt="">
+  <div id="akreditasi" class="container text-center mt-5" style="margin-bottom: 200px;">
+    <h2 class="py-2">Akreditasi Program Studi S2 Sistem Informasi</h2>
+    <p class="text-justify">
+      Program Magister S2 Sistem Informasi di ITB STIKOM Bali berkomitmen untuk menyediakan pendidikan berkualitas tinggi yang diakui oleh lembaga akreditasi nasional. Program ini telah memperoleh akreditasi dari Badan Akreditasi Nasional Perguruan Tinggi (BAN-PT) dengan peringkat "Baik".
+    </p> 
+    <p class="text-justify">
+      Untuk informasi lebih lanjut, Anda dapat mengakses dokumen resmi melalui tautan berikut:
+    </p> 
+    <ul class="text-justify">
+      <li>
+          <a href="https://stmikstikombali-my.sharepoint.com/:b:/r/personal/si_pasca_stikom-bali_ac_id/Documents/DOKUMEN%20PASCASARJANA/SK%20Akreditasi%20Program%20Studi%20.pdf?csf=1&web=1&e=3OqcRM">
+           <p>Surat Keputusan Akreditasi "Baik" dari BAN-PT</p> 
+          </a>
+      </li>
+      <li>
+          <a href="https://stmikstikombali-my.sharepoint.com/:b:/g/personal/si_pasca_stikom-bali_ac_id/ET_C3nmpN3lLtYMmnp2jn-cBpaXzpvpJQNrWhdbbGeq_iw?e=dkzIfi">
+            <p>Surat Keputusan Izin Pembukaan Program Studi S2 Sistem Informasi dari Kemendikbud</p>
+          </a>
+      </li>
+    </ul>
+    <p class="text-justify mb-5">
+      Kami terus berupaya untuk meningkatkan mutu pendidikan dan menghasilkan lulusan yang kompeten serta siap bersaing di dunia industri dan akademik.
+    </p> 
   </div>
+
 
   <!-- footer -->
   <x-footer></x-footer>
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
+    crossorigin="anonymous"></script>
 
 </body>
 

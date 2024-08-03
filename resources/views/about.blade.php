38972ce329dd32acc0e6b67b1c4918c867977ab3
Pande Pratama <pratamaartha11@gmail.com>
2024-08-03 12:12:52 +0800

    update slide show landing page, transition image

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
-        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="css/style.css">
     <!-- AOS CSS -->
@@ -21,35 +20,35 @@
     <x-navbar></x-navbar>
     <!-- End Navbar -->
 
-    <section id="landingpage" class="container-fluid textColorPrimary custom-section"
-        style="padding: 60px 20px 0px 80px; position: relative; background-color:#202C59; color:white; min-height:100vh;">
-        <img src="img/ornamen.png" alt="Corner Image" class="corner-image top-left"
-            style="position: absolute; width: 300px; height: 300px; transform:rotate(180deg); top: 0; left:0;"
-            data-aos="fade-in">
+    <section id="landingpage" class="container-fluid textColorPrimary custom-section" style="padding: 60px 20px 0px 80px; position: relative; background-color:#202C59; color:white; min-height:100vh;">
+        <img src="img/ornamen.png" alt="Corner Image" class="corner-image top-left" style="position: absolute; width: 300px; height: 300px; transform:rotate(180deg); top: 0; left:0;" data-aos="fade-in">
 
-        <div class="row align-items-center">
-            <div class="col-md-6">
+        <div class="row d-flex align-items-center">
+            <div class="col-md-6 d-flex flex-column justify-content-center">
                 <h3 class="ml-3" data-aos="fade-up">Program Pascasarjana</h3>
                 <h1 class="ml-3" data-aos="fade-up">MAGISTER KOMPUTER (S2) PROGRAM STUDI SISTEM INFORMASI</h1>
-                <p class="ml-3" data-aos="fade-up">Institut Teknologi & Bisnis (ITB) STIKOM Bali<br>Nomor Akreditasi:
-                    3316/SK/BAN-PT/Ak.P/M/VIII/2023</p>
-                <a href="https://siap.stikom-bali.ac.id/" class="btn btn-danger mb-2 ml-3" data-aos="fade-up">Daftar
-                    Sekarang</a>
+                <p class="ml-3" data-aos="fade-up">Institut Teknologi & Bisnis (ITB) STIKOM Bali<br>Nomor Akreditasi: 3316/SK/BAN-PT/Ak.P/M/VIII/2023</p>
+                <a href="https://siap.stikom-bali.ac.id/" class="btn btn-danger mb-2 ml-3 px-0" data-aos="fade-up">Daftar Sekarang</a>
             </div>
 
-            <div class="col-md-6 text-center py-5">
-                <img src="img/gedung4.jpg" alt="Image" class="img-fluid mr-5 zoom-in"
-                    style="height: 400px; width: 500px; border-radius: 15px;">
+            <div class="col-md-6 text-center py-5 d-flex justify-content-center align-items-center">
+                <div class="slideshow-container">
+                    <div class="slideshow-inner">
+                        <img src="img/gedung4.jpg" alt="Image 1" class="img-fluid slideshow-image">
+                        <img src="img/ajar1.jpg" alt="Image 2" class="img-fluid slideshow-image">
+                        <img src="img/bssn1.jpg" alt="Image 3" class="img-fluid slideshow-image">
+                    </div>
+                </div>
             </div>
         </div>
 
-        <img src="img/ornamen.png" alt="Corner Image" class="corner-image bottom-right"
-            style="position: absolute; width: 300px; height: 300px; bottom:0; right:0;">
+        <img src="img/ornamen.png" alt="Corner Image" class="corner-image bottom-right" style="position: absolute; width: 300px; height: 300px; bottom:0; right:0;">
     </section>
+
     <!-- end landingpage -->
 
     <!-- sambutan rektor -->
-    <div id="sambutan" class="container mt-5" >
+    <div id="sambutan" class="container mt-5">
         <div class="row d-flex align-items-stretch">
             <div class="col-sm-4 d-flex align-items-center justify-content-center">
                 <div class="card-borderless" style="width: 25rem;">
@@ -63,7 +62,7 @@
                 <section>
                     <div>
                         <div>
-                            <h2  class="mt-3 text-center">Sambutan Rektor</h2>
+                            <h2 class="mt-3 text-center">Sambutan Rektor</h2>
                             <div class="content-paragraph mx-3 mx-md-5 text-justify" style="font-size: large;">
                                 <p>Salam STIKOM Bali,</p>
                                 <p>Selamat datang di Program Magister Komputer Prodi S2 Sistem Informasi, ITB STIKOM Bali. Kami sangat bangga memperkenalkan program Strata dua S2, yaitu Magister Komputer Sistem Informasi, yang merupakan Prodi S2 Sistem Informasi satu-satunya di Bali, dan saat ini memiliki akreditasi Baik oleh Ban-PT. Sesuai visi Institut Teknologi dan Bisnis (ITB) STIKOM Bali yaitu menjadi perguruan tinggi unggulan dan berkualitas Internasional di bidang ilmu pengetahuan, teknologi dan seni. Kurikulum Program Studi S2 Sistem Informasi dirancang khusus untuk memenuhi kebutuhan dunia industri dan akademik yang terus berkembang, dengan empat konsentrasi utama: Cyber Security, Data Science, Enterprise System, dan Medical Informatic, sehingga mampu mewujudkan kualitas lulusan yang memiliki daya saing global secara nasional dan internasional.</p>
@@ -90,7 +89,7 @@
                     <div>
                         <div>
                             <div class="atur mr-5 mt-5">
-                                <h2  class="mt-3 text-center">Institut Teknologi dan Bisnis STIKOM Bali</h2>
+                                <h2 class="mt-3 text-center">Institut Teknologi dan Bisnis STIKOM Bali</h2>
                                 <p class="content-paragraph text-justify mx-3 mx-md-5 " style="font-size: large;">
                                     Berawal dari bertemunya para pemerhati, pencinta dan praktisi pendidikan yakni Prof.
                                     Dr. Made Bandem, MA., (saat itu Rektor ISI Jogjakarta), Dr. Dadang Hermawan
@@ -120,16 +119,14 @@
                 </section>
             </div>
             <div class="col-sm-4 d-flex align-items-center justify-content-center">
-                <img src="/img/gedunglens.jpg" class="mx-auto p-2 img-fluid" style="border-radius: 20px"
-                    alt="ITB STIKOM Bali Logo">
+                <img src="/img/gedunglens.jpg" class="mx-auto p-2 img-fluid" style="border-radius: 20px" alt="ITB STIKOM Bali Logo">
             </div>
         </div>
     </div>
 
     <!-- Visi Misi Tujuan -->
     <section id="visi-misi" class="text-light position-relative" style="background-color: #202C59; position: relative; ">
-        <img src="img/ornamen.png" alt="Corner Image" class="corner-image top-image"
-            style="transform: rotate(180deg); width: 300px; height: 300px; position: absolute; top: 0; left: 0;">
+        <img src="img/ornamen.png" alt="Corner Image" class="corner-image top-image" style="transform: rotate(180deg); width: 300px; height: 300px; position: absolute; top: 0; left: 0;">
         <div class="container py-5 mt-3">
             <br>
             <div class="text-center mt-4 ">
@@ -137,8 +134,8 @@
             </div>
             <br>
             <h3>Visi</h3>
-                <p>Menjadi penyelenggara program Pascasarjana yang berkualitas dan berdaya saing internasional dalam
-                    pengembangan bidang ilmu Pengetahuan, Teknologi dan Seni</p>
+            <p>Menjadi penyelenggara program Pascasarjana yang berkualitas dan berdaya saing internasional dalam
+                pengembangan bidang ilmu Pengetahuan, Teknologi dan Seni</p>
             <br>
             <div>
                 <h3>Misi</h3>
@@ -151,8 +148,7 @@
                     kebermanfaatan bagi mahasiswa.
                 </p>
             </div>
-            <img src="img/ornamen.png" alt="Corner Image" class="corner-image bottom-image"
-                style="width: 300px; height: 300px; position: absolute; bottom: 0; right: 0;">
+            <img src="img/ornamen.png" alt="Corner Image" class="corner-image bottom-image" style="width: 300px; height: 300px; position: absolute; bottom: 0; right: 0;">
     </section>
 
     <!-- Struktur Organisasi -->
@@ -161,8 +157,7 @@
         <h5>Pascasarjana ITB STIKOM Bali</h5>
         <br>
         <div class=" rounded d-inline-block">
-            <img src="img/STRUKTURPASCA.jpg" alt="Struktur Organisasi" class="img-fluid"
-                style="width: 1100px; height: 1600px;">
+            <img src="img/STRUKTURPASCA.jpg" alt="Struktur Organisasi" class="img-fluid" style="width: 1100px; height: 1600px;">
         </div>
     </div>
 
@@ -176,24 +171,24 @@
         <div class="img-container">
             <div class="img-wrapper">
                 <img src="img/2.jpg" alt="Dosen 1">
-                <span class="img-name">Prof. Dr. I Made Bandem</span>
+                <figcaption>Prof. Dr. I Made Bandem</figcaption>
             </div>
             <div class="img-wrapper">
                 <a href="https://scholar.google.com/citations?hl=id&user=o5p089MAAAAJ">
                     <img src="img/8.jpg" alt="Dosen 2">
-                    <span class="img-name">Dr. Dadang Hermawan</span>
+                    <figcaption>Dr. Dadang Hermawan</figcaption>
                 </a>
             </div>
             <div class="img-wrapper">
                 <a href="https://scholar.google.com/citations?user=HGeM9xwAAAAJ&hl=id&oi=ao">
                     <img src="img/6.jpg" alt="Dosen 3">
-                    <span class="img-name">Dr. Roy Rudolf Huizen, S.T., M.T.</span>
+                    <figcaption>Dr. Roy Rudolf Huizen, S.T., M.T.</figcaption>
                 </a>
             </div>
             <div class="img-wrapper">
                 <a href="https://scholar.google.com/citations?user=igzQmigAAAAJ&hl=id&oi=ao">
                     <img src="img/3.jpg" alt="Dosen 4">
-                    <span class="img-name">Dr. Dandy Pramana Hostiadi, S.Kom., M.T.</span>
+                    <figcaption>Dr. Dandy Pramana Hostiadi, S.Kom., M.T.</figcaption>
                 </a>
             </div>
         </div>
@@ -201,25 +196,25 @@
             <div class="img-wrapper">
                 <a href="https://scholar.google.com/citations?user=0icLc7QAAAAJ&hl=id&oi=ao">
                     <img src="img/5.jpg" alt="Dosen 5">
-                    <span class="img-name">Dr. Gede Angga Pradipta, S.T., M.Eng.</span>
+                    <figcaption>Dr. Gede Angga Pradipta, S.T., M.Eng.</figcaption>
                 </a>
             </div>
             <div class="img-wrapper">
                 <a href="https://scholar.google.com/citations?user=cSku1GYAAAAJ&hl=id&oi=ao">
                     <img src="img/4.jpg" alt="Dosen 6">
-                    <span class="img-name">Dr. Evi Triandini, S.P., M.Eng.</span>
+                    <figcaption>Dr. Evi Triandini, S.P., M.Eng.</figcaption>
                 </a>
             </div>
             <div class="img-wrapper">
                 <a href="https://scholar.google.com/citations?user=fFjHpfAAAAAJ&hl=id&oi=ao">
                     <img src="img/1.jpg" alt="Dosen 7">
-                    <span class="img-name">Dr. Putu Desiana Wulaning Ayu., S.T., M.T.</span>
+                    <figcaption>Dr. Putu Desiana Wulaning Ayu., S.T., M.T.</figcaption>
                 </a>
             </div>
             <div class="img-wrapper">
                 <a href="https://scholar.google.com/citations?user=98py7mQAAAAJ&hl=id&oi=ao">
                     <img src="img/7.jpg" alt="Dosen 8">
-                    <span class="img-name">Dr. Putri Srinadi, S.E, M.M.Kom.</span>
+                    <figcaption>Dr. Putri Srinadi, S.E, M.M.Kom.</figcaption>
                 </a>
             </div>
         </div>
@@ -230,12 +225,8 @@
     <x-footer></x-footer>
     <!-- End Footer -->
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2LcSo/jwBFOjLl3Fw5yTzgJOfdwtFP7iomWtpj6MO9"
-        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2LcSo/jwBFOjLl3Fw5yTzgJOfdwtFP7iomWtpj6MO9" crossorigin="anonymous"></script>
 
     <!-- Existing body content -->
     <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
@@ -248,7 +239,7 @@
 
 
     <script>
-        document.addEventListener('DOMContentLoaded', function () {
+        document.addEventListener('DOMContentLoaded', function() {
             const elements = document.querySelectorAll('.zoom-in, .fade-in');
 
             function checkInView() {
@@ -263,6 +254,21 @@
             checkInView(); // Initial check
         });
     </script>
+
+    <script>
+        let currentIndex = 0;
+        const slideshowInner = document.querySelector('.slideshow-inner');
+        const images = document.querySelectorAll('.slideshow-image');
+
+        function showNextImage() {
+            currentIndex = (currentIndex + 1) % images.length;
+            slideshowInner.style.transform = `translateX(-${currentIndex * 100}%)`;
+        }
+
+        setInterval(showNextImage, 3000); // Ganti gambar setiap 3 detik
+    </script>
+
+
 
 </body>
 
